3ce7dff4f8120c35bd4924679dccc76a4ffbe18a
sheikhilyas <sheikhilyas031631@gmail.com>
2024-11-14 21:42:02 +0500

    Adding

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,26 +7,26 @@ function Hero() {
       <div className="header-boxes-con">
         {/* Left */}
         <div>
-          <Image 
-            src="/images/sunny-copy.png" 
-            alt="Profile" 
-            width={300}
-            height={300}
-            className="header-image"
-          />
+        <Image 
+    src="/images/sunny-copy.png" 
+    alt="Profile" 
+    width={300}
+    height={300}
+    className="header-image"
+/>
         </div>
 
         {/* Right */}
         <div className="hero-right-div">
           <h1 className="title-hero">
-            I'm Sheikh Ilyas <br /> Front-end Developer
+            I&apos;m Sheikh Ilyas <br /> Front-end Developer
           </h1>
           <p className="des-header">
-            I'm Sheikh Ilyas, a front-end developer passionate about building
+            I&apos;m Sheikh Ilyas, a front-end developer passionate about building
             beautiful, responsive websites. With expertise in Next.js, HTML, CSS,
             JavaScript, and frameworks like React, I focus on delivering seamless user
             experiences. I create websites that are not only visually appealing but
-            also highly functional. Lets work together to turn your ideas into
+            also highly functional. Let&apos;s work together to turn your ideas into
             interactive web solutions. Feel free to explore my work and get in touch!
           </p>
           <button className="hero-button">HIRE ME</button>
