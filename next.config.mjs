3c9d4069afa3744247800875678218820383dd97
sheikhilyas <sheikhilyas031631@gmail.com>
2024-11-17 11:24:15 +0500

    adding

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['your-image-domain.com'], 
-    },
-  };
-  
+  images: {
+    domains: ['example.com'], // External domains allowed for images
+  },
+};
+
 export default nextConfig;
