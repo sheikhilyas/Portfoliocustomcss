ee12ad068099b9cc46bff59652783d9fb7efadfa
sheikhilyas <sheikhilyas031631@gmail.com>
2024-11-17 12:28:46 +0500

    addind

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
+  
 
-export default nextConfig;
